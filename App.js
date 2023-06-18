4dd3f901c27c739e8f087674cde0d2ad55a47a09
Severo-Yannick <severoyannick@gmail.com>
2023-06-18 11:19:39 +0200

    current weather

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Feather name="sun" size={100} color="black" />
+        <Text style={styles.temp}>36°</Text>
+        <Text style={styles.feels}>Feels like 38°</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 40°</Text>
+          <Text style={styles.highLow}>Low: 22°</Text>
+        </View>
+      </View>
+      <View style={styles.bodyWrapper}>
+        <Text style={styles.description}>Its sunny</Text>
+        <Text style={styles.message}>Its perfect t-shirt weather</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: "pink",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  temp: {
+    color: "black",
+    fontSize: 48,
+  },
+  feels: {
+    color: "black",
+    fontSize: 30,
+  },
+  highLowWrapper: {
+    flexDirection: "row",
+  },
+  highLow: {
+    color: "black",
+    fontSize: 20,
+  },
+  bodyWrapper: {
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    paddingLeft: 25,
+    marginBottom: 40,
+  },
+  description: {
+    color: "black",
+    fontSize: 48,
+  },
+  message: {
+    color: "black",
+    fontSize: 30,
   },
 });
