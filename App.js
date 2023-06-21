9f8146b2deb0fb4fafcd65a2ed7e7fabbfe7ebaa
Severo-Yannick <severoyannick@gmail.com>
2023-06-21 16:02:41 +0200

    loading

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import Tabs from "./src/components/Tabs";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { useState } from "react";
 
 const App = () => {
-  return (
+  const [loading, setLoading] = useState(true);
+  return loading ? (
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="blue" />
+    </View>
+  ) : (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+  },
+});
 
 export default App;
