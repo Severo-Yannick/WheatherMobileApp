37ab205fc1d9ac1b3c2bd81ae08c857313a66dbe
Severo-Yannick <severoyannick@gmail.com>
2023-06-25 16:15:57 +0200

    Error screen

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import Tabs from "./src/components/Tabs";
 import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { useGetWeather } from "./src/hooks/useGetWeather";
+import ErrorItem from "./src/components/ErrorItem";
 
 const App = () => {
   const [loading, error, weather] = useGetWeather();
-  if (weather && weather.list) {
+
+  if (weather && weather.list && !loading) {
     return (
       <NavigationContainer>
         <Tabs weather={weather} />
@@ -14,7 +16,11 @@ const App = () => {
   }
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color="blue" />
+      {error ? (
+        <ErrorItem />
+        ) : (
+        <ActivityIndicator size="large" color="blue" />
+      )}
     </View>
   );
 };
